8f77074705a00c2e524f460b35a818656069974a
HANS KREBS <hans.krebs@mercedes-benz.com>
2023-07-27 08:50:45 +0200

    Add KeysOf helper type

--- src/types/types.ts
+++ src/types/types.ts
@@ -3,3 +3,16 @@ export type Prettify<T> = {
 } & {};
 
 export type ValueOf<T> = T[keyof T];
+
+/**
+ * __New type with keys of a given type__
+ *
+ * @example
+ *
+ * const Foo = { A: "a", B: "b"};
+ * type FooKeys = Keys<typeof Foo>;
+ * // type FooKeys = "A" | "B"
+ *
+ * equivalent to: type FooKeys = keyof typeof Foo;
+ */
+export type KeysOf<T> = keyof T;
