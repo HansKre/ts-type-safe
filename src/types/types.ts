751eddeec91a04098d8708064898857020e4bdc7
HANS KREBS <hans.krebs@mercedes-benz.com>
2023-07-27 09:00:57 +0200

    Add jsdoc to helper types

--- src/types/types.ts
+++ src/types/types.ts
@@ -1,18 +1,38 @@
+/**
+ * __Helper type to show all properties of a complex base-type__
+ *
+ * Constructed type is 100% compatible and assignable to base-type
+ *
+ */
 export type Prettify<T> = {
   [K in keyof T]: T[K];
 } & {};
 
-export type ValueOf<T> = T[keyof T];
+/**
+ * __Helper type generates values of a given type__
+ *
+ * NOTE: not for enum-types!
+ * Use type EnumVals = \`${EnumType}\`; to create values of an enum-type
+ *
+ * @example
+ *
+ * const Foo = { A: "a", B: "b"} as const;
+ * type FooVals = ValuesOf<typeof Foo>;
+ * // type FooVals = "a" | "b"
+ *
+ * // equivalent to: type FooVals = (typeof Foo)[keyof typeof Foo];
+ */
+export type ValuesOf<T> = T[keyof T];
 
 /**
- * __New type with keys of a given type__
+ * __Helper type generates keys of a given type__
  *
  * @example
  *
  * const Foo = { A: "a", B: "b"};
- * type FooKeys = Keys<typeof Foo>;
+ * type FooKeys = KeysOf<typeof Foo>;
  * // type FooKeys = "A" | "B"
  *
- * equivalent to: type FooKeys = keyof typeof Foo;
+ * // equivalent to: type FooKeys = keyof typeof Foo;
  */
 export type KeysOf<T> = keyof T;
