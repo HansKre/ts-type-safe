bcf59d4f8372ef4530a25181b11c5d1aba1222f9
HANS KREBS <hans.krebs@mercedes-benz.com>
2023-07-27 09:13:59 +0200

    Add isEnumValueWithNumbers

--- src/validators/validators.ts
+++ src/validators/validators.ts
@@ -102,3 +102,58 @@ export function isEnumValue<T extends Record<string, unknown>>(
 ): value is T[keyof T] {
   return Object.values(enumType).includes(value as T[keyof T]);
 }
+
+/**
+ *
+ * @example
+ *
+ * // The following enum is considered a "numbers-enum"
+ * enum E {
+ *   A,
+ *   B,
+ *   C,
+ * }
+ *
+ * // enum-keys are also values
+ * console.log(Object.values(E));
+ * // ["A", "B", "C", 0, 1, 2]
+ */
+export function isEnumValueWithNumbers<T extends Record<string, unknown>>(
+  enumType: T,
+  value: unknown
+): value is T[keyof T] {
+  const valuesAndMaybeKeys = Object.values(enumType);
+  // filter out keys from values
+  // TODO: below code does not work if keys and values have same value
+  // TODO: better approach: filter out all digits: if the filteredOutArr has half length of original array -> we have a numbers-enum -> take the numbers-arr as values, else use above method.
+  const onlyValues = valuesAndMaybeKeys.filter(
+    (v) => !Object.keys(enumType).includes(v as any)
+  );
+  return onlyValues.includes(value as T[keyof T]);
+}
+
+function isEnumValueNum<T extends Record<string, unknown>>(
+  enumType: T,
+  value: unknown
+): value is T[keyof T] {
+  // keys cannot be numbers
+  // even in case of numbers-enums like `enum N = { A, B, C}`,
+  // which yields `console.log(Object.keys(N))` as `// ["0", "1", "2", "A", "B", "C"]`,
+  // we are able to extract actual keys
+  // in a mixed scenario with non-number-values, we'll have values in `maybeKeys`, but
+  // these values will not be able to map in `enumType[k]`
+  const maybeKeys = Object.keys(enumType).filter((k) => {
+    return !/^\d/.test(k);
+  });
+  const values = maybeKeys.map((k) => {
+    return enumType[k];
+  });
+  // console.log("values", values);
+  return values.includes(value);
+}
+
+// console.log(isEnumValueNum(Num3, "foo"));
+// console.log(isEnumValueNum(Num3, 1));
+// console.log(isEnumValueNum(MyEnum, "thing one"));
+// console.log(isEnumValueNum(NumX, 1));
+// console.log(isEnumValueNum(E, 1));
