1f7387fb47fc409ed9c1d574cf0815e608dc08c4
HANS KREBS <hans.krebs@mercedes-benz.com>
2024-08-19 15:14:26 +0200

    Add DeepRequired and fix and rename KeysOfWithNestedPrefix

--- src/types/types.ts
+++ src/types/types.ts
@@ -20,6 +20,47 @@ export type Prettify<T> = {
 STUB = 1;
 
 /**
+ * Helper type to recursively remove all '?' and 'undefined' from all properties
+ *
+ * @example
+ *
+ * type Settings = {
+ *   org: string;
+ *   repo?: string;
+ *   owner?: {
+ *       profileUrl?: string;
+ *       contact: {
+ *         name: string;
+ *         mail: string;
+ *       } | undefined;
+ *   };
+ * };
+ * type ReqSettings = DeepRequired<Settings>;
+ * // type ReqSettings = {
+ * //     org: string;
+ * //     repo: string;
+ * //     owner: {
+ * //         profileUrl: string;
+ * //         contact: {
+ * //             name: string;
+ * //             mail: string;
+ * //         };
+ * //     };
+ * // }
+ *
+ * @typedef {DeepRequired} DeepRequired<T>
+ */
+export type DeepRequired<T> = T extends Function
+  ? T
+  : T extends Array<infer U>
+  ? Array<DeepRequired<U>>
+  : T extends object
+  ? { [K in keyof T]-?: DeepRequired<NonNullable<T[K]>> }
+  : T;
+
+STUB = 1;
+
+/**
  * Helper type to generate values of a given type
  *
  * NOTE: not for enum-types!
@@ -61,38 +102,41 @@ export type KeysOf<T> = keyof T;
 
 STUB = 1;
 
+// Below type does not work with nested optional or undefined properties, hence it is an intermediate helper to be composed with DeepRequired
+type DeepKeysOfHelper<TBase, TPrefix extends string = ''> = {
+  [K in keyof TBase]-?: TBase[K] extends object
+    ? K extends string
+      ? `${TPrefix}${K}` | DeepKeysOfHelper<TBase[K], `${TPrefix}${K}.`>
+      : never
+    : K extends string
+    ? `${TPrefix}${K}`
+    : never;
+}[keyof TBase];
+
 /**
- * Helper type to generate a new type holding all the keys of a given type including the nested keys which are prefixed by the parent property's key.
- *
- * IMPORTANT: works only for nested objects which are not optional or potentially undefined!
+ * Helper type recursively generates a union type of all keys of a given type. The nested keys are prefixed by the parent property's key.
  *
  * @example
  *
  * type Settings = {
  *   org: string;
  *   repo?: string;
- *   owner: {
+ *   owner?: {
  *       profileUrl?: string;
  *       contact: {
  *         name: string;
  *         mail: string;
- *       };
+ *       } | undefined;
  *   };
  * };
- * type SettingKey = KeysOfWithNestedPrefix<Settings>;
+ * type SettingKey = DeepKeysOf<Settings>;
  * // type SettingKey = "org" | "owner" | "owner.contact" | "owner.contact.name" | "owner.contact.mail" | "owner.profileUrl" | "repo";
  *
- * @typedef {KeysOfWithNestedPrefix} KeysOfWithNestedPrefix<TBase, TPrefix extends string = ''>
+ * TypeScript Playground Link: https://www.typescriptlang.org/pt/play/?#code/C4TwDgpgBA0hIGcDyAzA6gS2ACwHIQWAgBMAFAJwhQwA8AeAFQCEBDBCAGigYqtqgg0iAO2IIohchmEBzKAF4oAciUA+BVADeAKChQA2jCjSoAa3gB7FN1bsAugFoA-AC4bbCIbsChEUeIsAIwArCABjYF09KCdYHxExCWApWSjomKgAAwASTR5KahoAX1yYIsyoAB9YeGR0LDwCIjIC2kZbTxg7Lhy83kKSzTKAOkzVNOi3YQgANwhyCbcjQQTxSWkZCdje-L5i0vLFqGm58gBubSL9cxArd3sL7VBIKABlCGBgDfFFHT0LcgyNzrWQXPSUMAWVxJFIyMFQCwAd2m5Ghf3SUDA5CsGAANhAAKrkXHQkFwiZ6MIWYTAFgRNzojHHFgAWwgwOSG3hTJZLDxHNh3OiRSqUAArqI+NNiPCihc5donuBoAARCAQMAAJQgAEcxRhKMRGOpFAx4n5EgAxCURDDUqKxBhRNxmlYW8QAQXI5BYIDo0hQ8ygBPGeliXp9frVGu1eoNJDoIdDUBd5v8CJC4UiYa0BiMJhudwYjmh0a1uv1hrouGpuDFuNxLEC+MYXlU6iKzu4j2e0Fj70+3w0ZdjlYTA6+sgQqh7yreH0nMjgIA0y7qmBw+EIJH6bRHFfjRon33bjypwkIEgXG2XbmPsmXGgAREiUcNz7SIsNeXin2cgA
+ *
+ * @typedef {DeepKeysOf} DeepKeysOf<TBase, TPrefix extends string = ''>
  */
-export type KeysOfWithNestedPrefix<TBase, TPrefix extends string = ''> = {
-  [K in keyof TBase]-?: TBase[K] extends object
-    ? K extends string
-      ? `${TPrefix}${K}` | KeysOfWithNestedPrefix<TBase[K], `${TPrefix}${K}.`>
-      : never
-    : K extends string
-    ? `${TPrefix}${K}`
-    : never;
-}[keyof TBase];
+export type DeepKeysOf<T> = DeepKeysOfHelper<DeepRequired<T>>;
 
 STUB = 1;
 
